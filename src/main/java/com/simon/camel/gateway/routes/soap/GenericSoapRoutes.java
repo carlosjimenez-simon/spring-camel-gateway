f1e8d73fc2508ff1e44b154f7b1ffc241b4e3e57
cjimenezv@gmail.com <cjimenezv@gmail.com>
2026-04-16 08:42:01 -0500

    caj., ajustes a la base de soap

--- src/main/java/com/simon/camel/gateway/routes/soap/GenericSoapRoutes.java
+++ src/main/java/com/simon/camel/gateway/routes/soap/GenericSoapRoutes.java
@@ -11,32 +11,35 @@ public class GenericSoapRoutes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        // 1. Definición del REST DSL
+    	// 1. La URL
         rest("/api/v1")
-            .post("/seguros/{operacion}")
+        	.post("/gateway-to/{organizacion}/{operacion}")
                 .routeId("generic-soap-gateway")
-                // Aquí ya no definimos .type() porque queremos que sea GENÉRICO (Map)
                 .to("direct:procesar-plantilla");
 
-        // 2. Lógica Maestra con Velocity
+        // 2. Lógica Maestra
+     // 2. Lógica Maestra
         from("direct:procesar-plantilla")
-	        .routeId("logic-velocity")
-	        
-	        // 1. ELIMINAMOS EL UNMARSHAL. 
-	        // Si Camel ya lo convirtió a Mapa, esto sobra. 
-	        // Si por casualidad llegara como String, Camel lo convierte a Mapa automáticamente al usar Velocity si es necesario.
-	        
-	        .log("Datos que llegan a la plantilla: ${body}")
-	        
-	        // 2. Cargamos la plantilla
-	        .toD("velocity:templates/${header.operacion}.vm")
-	        
-	        // 3. Headers y envío
-	        .removeHeaders("CamelHttp*")
-	        .setHeader("Content-Type", constant("application/xml"))
-	        .log("XML generado: ${body}")
-	        
-	        .to("http://localhost:8081/ws/clientes?bridgeEndpoint=true")
-	        .log("Respuesta: ${body}");
+            .routeId("logic-velocity")
+            
+            // Aseguramos que el body sea un Mapa para Velocity
+            .convertBodyTo(Map.class)
+            
+            .log("Procesando Org: ${header.organizacion} - Op: ${header.operacion}")
+            
+            // 2. Cargamos la plantilla dinámicamente usando AMBOS headers
+            // La ruta final será: templates/test/clientes.vm o templates/seguros_mundial/expedir.vm
+            .toD("velocity:templates/${header.organizacion}/${header.operacion}.vm")
+            
+            // 3. Headers y envío
+            .removeHeaders("CamelHttp*")
+            .setHeader("Content-Type", constant("application/xml"))
+            
+            // TIP: Aquí podrías usar un switch o un bean para cambiar la URL de destino 
+            // según la organización si no quieres enviarlo todo al mismo sitio.
+            .log("XML generado para ${header.organizacion}: ${body}")
+            
+            .toD("${properties:simon.endpoint.${header.organizacion}.${header.operacion}}?bridgeEndpoint=true")
+            .log("Respuesta recibida: ${body}");
     }
 }
