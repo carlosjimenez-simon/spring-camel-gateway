82a0a9e4afe861f2333c02c2adda2ac7625c9dbd
carlosjimenez-simon <carlos.jimenez@simonmovilidad.com>
2026-07-15 12:43:42 -0500

    caj, se ajuustan los end-point de assist ya

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 17 Jun 2026 15:01");
+		log.info("Version 15 Jul 2026 15:01");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
