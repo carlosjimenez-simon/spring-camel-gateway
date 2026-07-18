7d1ca1e61a81627310bfce72a38a9f0d4aa03c17
cjimenezv@gmail.com <cjimenezv@gmail.com>
2026-07-18 14:43:48 -0500

    caj, ajustes back assistya

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 18 Jul 2026 14:07");
+		log.info("Version 18 Jul 2026 14:40");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
