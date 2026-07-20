8d43f673e62190511d6f0919930d9f16f06b00b3
cjimenezv@gmail.com <cjimenezv@gmail.com>
2026-07-20 09:44:36 -0500

    caj, se realizan ajustes para variables de core y external

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 18 Jul 2026 14:40");
+		log.info("Version 20 Jul 2026 09:38");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
--- src/main/java/com/simon/camel/gateway/routes/GatewayRoutes.java
+++ src/main/java/com/simon/camel/gateway/routes/GatewayRoutes.java
@@ -2,10 +2,17 @@ package com.simon.camel.gateway.routes;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GatewayRoutes extends RouteBuilder {
+	
+	@Value("${netty.port:9000}")
+    private int nettyPort;
+	
+	@Value("${netty.context-path:/simon-sprint-camel}")
+    private String nettyContextPath;
 
 	@Override
     public void configure() throws Exception {
@@ -14,8 +21,8 @@ public class GatewayRoutes extends RouteBuilder {
         restConfiguration()
             .component("netty-http")  //defecto: servlet
             .host("0.0.0.0") // Permite conexiones desde cualquier IP
-            .port(9000)      // Puerto dedicado para el tráfico de alta velocidad
-            .contextPath("/simon-sprint-camel")
+            .port(nettyPort)      // Puerto dedicado para el tráfico de alta velocidad
+            .contextPath(nettyContextPath)
             .bindingMode(RestBindingMode.auto);
     }
 }
