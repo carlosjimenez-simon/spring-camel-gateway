aaee2c10bbf0666f8e54b96242525ee60b1b26e1
carlosjimenez-simon <carlos.jimenez@simonmovilidad.com>
2026-07-30 09:38:09 -0500

    caj, ajustes composicion

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 24 Jul 2026 12:37");
+		log.info("Version 30 Jul 2026 09:20");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
