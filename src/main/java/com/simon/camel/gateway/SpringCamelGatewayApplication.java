84c12c0d914675c90e95344af0aeb13ce68b1683
angelamarillo-simon <angel.amarillo@simonmovilidad.com>
2026-09-03 14:22:32 -0500

    AA, Se crea la operación de Finanzauto get-departments en los yml

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 01 Sept 2026 17:37");
+		log.info("Version 03 Sept 2026 12:42");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
