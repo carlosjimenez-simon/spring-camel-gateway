794848ee538596728851833e21199234031a3691
angelamarillo-simon <angel.amarillo@simonmovilidad.com>
2026-09-08 14:37:36 -0500

    AA, Update application.yml (dev, UAT, PROD)

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 03 Sept 2026 12:42");
+		log.info("Version 08 Sept 2026 14:36");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
