90aa533d0dafe4e396531462bfa1380ed682fd85
IvanDSimon <ivan.penaloza@simonmovilidad.com>
2026-09-15 14:52:56 -0500

    ip, camel-core updated with finerack uat

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 14 Sept 2026 16:30");
+		log.info("Version 15 Sept 2026 14:01");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
