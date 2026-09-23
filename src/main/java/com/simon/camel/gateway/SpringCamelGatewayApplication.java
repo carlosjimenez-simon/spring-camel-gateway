76b52ac7f4ebada5c0fa02d6d066e788bee59b7b
IvanDSimon <ivan.penaloza@simonmovilidad.com>
2026-09-23 10:50:39 -0500

    ip, finandina mokups responses updated

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 21 Sept 2026 09:15");
+		log.info("Version 23 Sept 2026 10:40");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
