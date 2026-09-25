36a127d1296c78a27fc93aaeb898eb71e08e3e0a
IvanDSimon <ivan.penaloza@simonmovilidad.com>
2026-09-25 11:18:54 -0500

    ip, carsharing uat added

--- src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
+++ src/main/java/com/simon/camel/gateway/SpringCamelGatewayApplication.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringCamelGatewayApplication {
 
 	public static void main(String[] args) {
-		log.info("Version 24 Sept 2026 17:11");
+		log.info("Version 25 Sept 2026 11:10");
 		SpringApplication.run(SpringCamelGatewayApplication.class, args);
 	}
 
